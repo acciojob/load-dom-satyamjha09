ccf052e8986a36c9f694ff8f97f6dc7f34b8f6e1
satyam jha <109567398+satyamjha09@users.noreply.github.com>
2024-12-03 13:16:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+
+const Dom = document.getElementById("DOM");
+
+document.addEventListener("DOMContentLoaded", (event) => {
+	Dom.textContent = "DOM load success"
+});
